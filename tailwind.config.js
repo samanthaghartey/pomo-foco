f44c54e3410cdcd4a6a57b910ce0af780b1b020a
Code Gummy <codegummy33@gmail.com>
2024-12-08 01:51:23 +0100

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   daisyui: {
     themes: [
-      "light", "dark", "cupcake",
+      "light", 
+      "dark", 
+      "cupcake", 
       {
         mytheme: {
-          
-"primary": "#ff00ee",
-          
-"secondary": "#0000ff",
-          
-"accent": "#0000ff",
-          
-"neutral": "#05190a",
-          
-"base-100": "#252530",
-          
-"info": "#007cb6",
-          
-"success": "#3ba22e",
-          
-"warning": "#9b5800",
-          
-"error": "#d5004c",
-          },
+          "primary": "#ff00ee",
+          "secondary": "#0000ff",
+          "accent": "#0000ff",
+          "neutral": "#05190a",
+          "base-100": "#252530",
+          "info": "#007cb6",
+          "success": "#3ba22e",
+          "warning": "#9b5800",
+          "error": "#d5004c",
         },
-
-
-      ],
-    },
-  plugins: [ require("@tailwindcss/typography"), require('daisyui'),],
+      },
+    ],
+  },
+  plugins: [
+    require("@tailwindcss/typography"), 
+    require('daisyui'),
+  ],
 }
-
